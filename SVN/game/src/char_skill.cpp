8d76451a6fdf2c240a42bfc91af9b97c0676ea38
Robert <55710848+Rakancito@users.noreply.github.com>
2019-09-23 12:58:02 -0500

    Create char_skill.cpp

---
+++ SVN/game/src/char_skill.cpp
@@ -0,0 +1,35 @@
+// Search
+
+if (IS_SET(m_pkSk->dwFlag, SKILL_FLAG_REMOVE_GOOD_AFFECT))
+
+// Add before
+
+			if (IS_SET(m_pkSk->dwFlag, SKILL_FLAG_KNOCKBACK))
+			{
+				float fknockbackLength = 300; //Distance for KnockBack, defult 300
+        
+				float fx, fy;
+				float degree = GetDegreeFromPositionXY(m_pkChr->GetX(), m_pkChr->GetY(), pkChrVictim->GetX(), pkChrVictim->GetY());
+
+				if (m_pkSk->dwVnum == SKILL_HORSE_WILDATTACK)
+				{
+					degree -= m_pkChr->GetRotation();
+					degree = fmod(degree, 360.0f) - 180.0f;
+
+					if (degree > 0)
+						degree = m_pkChr->GetRotation() + 90.0f;
+					else
+						degree = m_pkChr->GetRotation() - 90.0f;
+				}
+
+				GetDeltaByDegree(degree, fknockbackLength, &fx, &fy);
+				sys_log(0, "KNOCKBACK! %s -> %s (%d %d) -> (%d %d)", m_pkChr->GetName(), pkChrVictim->GetName(), pkChrVictim->GetX(), pkChrVictim->GetY(), (long)(pkChrVictim->GetX()+fx), (long)(pkChrVictim->GetY()+fy));
+				long tx = (long)(pkChrVictim->GetX()+fx);
+				long ty = (long)(pkChrVictim->GetY()+fy);
+
+				pkChrVictim->Sync(tx, ty);
+				pkChrVictim->Goto(tx, ty);
+				pkChrVictim->CalculateMoveDuration();
+
+				pkChrVictim->SyncPacket();
+			}
